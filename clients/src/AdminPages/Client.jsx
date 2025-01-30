4452ab1c9a01f6a0c50bceecbe2e0f699485aaa1
Fenosoa <fenosoarp6@gmail.com>
2025-01-30 19:32:45 +0300

    modification du design du page client

--- clients/src/AdminPages/Client.jsx
+++ clients/src/AdminPages/Client.jsx
@@ -17,36 +17,43 @@ export default function Client(){
     }, []);
 
     return(
-        <div>
-            <NavAdmin/>
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-lg-3">
-                        <SideBar/>
-                    </div>
-                    <div className="col-lg-9">
-                        <table className="table">
-                            <thead>
-                                <tr>
-                                    <th>Clients</th>
-                                    <th>Email</th>
-                                </tr>
-                            </thead>
-                      
-                        <tbody>
-                            {clients.map((value , index)  => (
-                                <tr key={index}>
-                                    <td>{value.username}</td>
-                                    <td>{value.email}</td>
-                                </tr>
-                            ))}
-
-                        </tbody>
-                    </table>
-                    </div>
-                </div>
-
+    
+        <div className="container-fluid">
+        <div className="row">
+          <SideBar />
+          <div className="col-lg-10">
+            <div className="row">
+            <NavAdmin />
             </div>
+            
+            <div className="row">
+              <div className="col-lg-12">
+                <h2  style={{marginTop : '10px'}}>Clients</h2>
+                <table className="table">
+                             <thead>
+                                 <tr>
+                                     <th>Clients</th>
+                                     <th>Email</th>
+                                 </tr>
+                             </thead>
+                      
+                         <tbody>
+                             {clients.map((value , index)  => (
+                                 <tr key={index}>
+                                     <td>{value.username}</td>
+                                     <td>{value.email}</td>
+                                 </tr>
+                             ))}
+
+                         </tbody>
+                     </table>
+             
+                </div>
+                
+             
+            </div>
+          </div>
         </div>
+      </div>
     )
 }
